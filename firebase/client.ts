5d1fde717a604fcea1477c0f47beac08936a82a5
aryanraj0104 <aryanr.0104@gmail.com>
2025-09-14 22:25:45 +0530

    Firebase Setup and Sign in n up ui done

---
+++ firebase/client.ts
@@ -0,0 +1,21 @@
+import { getFirestore } from "firebase/firestore";
+import { getApp, getApps, initializeApp } from "firebase/app"
+import {getAuth} from 'firebase/auth'
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC14FVKLbH9asBXJRDSVaW-tFy8GSfauQw",
+  authDomain: "momentia-ed3df.firebaseapp.com",
+  projectId: "momentia-ed3df",
+  storageBucket: "momentia-ed3df.firebasestorage.app",
+  messagingSenderId: "843471341945",
+  appId: "1:843471341945:web:2d32715af79cc4f56a07a8",
+  measurementId: "G-CGGYPKK2H7"
+};
+
+// Initialize Firebase
+const app = !getApps.length?initializeApp(firebaseConfig):getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
