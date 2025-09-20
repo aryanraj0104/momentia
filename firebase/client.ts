c9e2937eea0b83178e7a5659a4e99e3613c4f4e0
aryanraj0104 <aryanr.0104@gmail.com>
2025-09-21 00:41:55 +0530

    implement auth and storage, sign-in/sign-up done

--- firebase/client.ts
+++ firebase/client.ts
@@ -5,7 +5,7 @@ import {getAuth} from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_CLIENT_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_API_KEY,
   authDomain: "momentia-ed3df.firebaseapp.com",
   projectId: "momentia-ed3df",
   storageBucket: "momentia-ed3df.firebasestorage.app",
